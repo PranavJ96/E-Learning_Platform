271a642e8db27ae0cb61038ae0ea740410ea6f09
Smruti Puranik <smrutip16@gmail.com>
2022-03-03 16:45:18 +0100

    merge issue

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import SideNav from './components/SideNav/SideNav'
 import Coursescreen from './components/Course_nav/Coursescreen';
 import Coursedetails from './components/Coursedetails/Coursedetails';
+
 function App() {
-  return (
-    <div style={{ backgroundImage: `url('/background.jpg')` }}  className="App">
-      <Coursedetails/>
-    </div>
-  );
+    return ( <
+        div style = {
+            { backgroundImage: `url('/background.jpg')` } }
+        className = "App" >
+        <
+        Coursedetails / >
+        <
+        /div>
+    );
 }
 
 export default App;
