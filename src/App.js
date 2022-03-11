a9d3664e53d9580804daffb04736ee71ac61f3c9
Smruti Puranik <smrutip16@gmail.com>
2022-03-11 16:53:44 +0100

    LiveClass Initialisation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
+import LiveClass from './components/LiveClass/LiveClass';
 
 
 function App() {
   return (
     <div style={{ backgroundImage: `url('/background.jpg')` }}  className="App">
-      
-      <Home/>
+      <LiveClass/>
       
     </div>
   );
---
+++ src/components/LiveClass/LiveClass.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './LiveClass.css';
+import SideNav from "../SideNav/SideNav";
+
+
+const LiveClass = () => {
+    return ( 
+    <div class="sideNav1">
+        sefsfsefsef 
+        <SideNav/>
+    </div>
+    );
+};
+
+export default LiveClass;
